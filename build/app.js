ae952a17e06ff79371a763a410b2f59b7227468d
Sushil Kandel <skandel@myune.edu.au>
2023-10-02 18:52:05 +1100

    Yarn

--- build/app.js
+++ build/app.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv").config());
+const dotenv_1 = __importDefault(require("dotenv"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const connectDB_1 = __importDefault(require("./utils/connectDB"));
 const authHandler_1 = __importDefault(require("./handler/authHandler"));
@@ -13,7 +13,7 @@ dotenv_1.default.config();
 //  Connect to database.
 (0, connectDB_1.default)();
 const app = (0, express_1.default)();
-const port = 8888;
+const port = process.env.PORT;
 app.use(express_1.default.json());
 app.use('/api/auth', authHandler_1.default);
 app.use('/api/games', gameHandler_1.default);
